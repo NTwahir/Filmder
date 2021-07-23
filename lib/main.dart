146d2ea57a21b86d73fa0e2b020f2ea8e077c7d8
20193907 <c.emok@student.tue.nl>
2021-07-24 00:51:39 +0200

    Authentication methods

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,12 @@ import 'package:filmder/screens/sign_in.dart';
 import 'package:filmder/screens/home.dart';
 import 'package:filmder/screens/sign_up.dart';
 import 'package:filmder/theme/colors.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MaterialApp(
     theme: ThemeData(),
     darkTheme: ThemeData.dark(),
