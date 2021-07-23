12c19a866b78e7791a2e35037e1ce5e5cd5345a5
Nagib <n.s.twahir@student.tue.nl>
2021-07-23 17:09:38 +0200

    movie card

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -14,28 +14,40 @@ class _HomeState extends State<Home> {
         backgroundColor: hexColor,
         title: Center(child: Text('Filmder')),
       ),
-      body: Panel(),
+      body: getBody(context),
       backgroundColor: accentColor,
     );
   }
 }
 
-class Panel extends StatefulWidget {
-  @override
-  _PanelState createState() => _PanelState();
+Widget getBody(context) {
+  var size = MediaQuery.of(context).size;
+  return Padding(
+    padding: const EdgeInsets.only(bottom: 120),
+    child: Container(
+      height: size.height,
+      child: MovieCard(),
+    ),
+  );
 }
 
-class _PanelState extends State<Panel> {
+class MovieCard extends StatefulWidget {
+  @override
+  _MovieCardState createState() => _MovieCardState();
+}
+
+class _MovieCardState extends State<MovieCard> {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Column(
-        children: [
-          Image.network(
-              'https://m.media-amazon.com/images/G/01/imdb/images/social/imdb_logo._CB410901634_.png'),
-          Text('movie name')
-        ],
+      semanticContainer: true,
+      elevation: 5,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
       ),
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      child: Image.network(
+          'https://m.media-amazon.com/images/G/01/imdb/images/social/imdb_logo._CB410901634_.png'),
     );
   }
 }
