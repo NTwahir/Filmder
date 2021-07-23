df5a9db71068cd19361b609de4f2804ab04301d4
Nagib <n.s.twahir@student.tue.nl>
2021-07-23 16:41:48 +0200

    working on home page

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -14,8 +14,28 @@ class _HomeState extends State<Home> {
         backgroundColor: hexColor,
         title: Center(child: Text('Filmder')),
       ),
-      body: Text('this is home page'),
+      body: Panel(),
       backgroundColor: accentColor,
+    );
+  }
+}
+
+class Panel extends StatefulWidget {
+  @override
+  _PanelState createState() => _PanelState();
+}
+
+class _PanelState extends State<Panel> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [
+          Image.network(
+              'https://m.media-amazon.com/images/G/01/imdb/images/social/imdb_logo._CB410901634_.png'),
+          Text('movie name')
+        ],
+      ),
     );
   }
 }
